e9bbea48507c29ab396a40c82308ba581bdd33ca
xko <germain.rgb@hotmail.fr>
2022-08-05 09:43:26 +0200

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-  get 'likes/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :gossips do 
     resources  :comments
     resources :likes, only: [:create, :destroy]
   end
+  resources :welcome, only: [:show]
+  resources :contacts, only: [:show]
   resources :cities, only: [:show]
   resources :users, only: [:show, :create, :new]
   resources :sessions, only: [:new, :create, :destroy]
-
+  resources :team, only: [:show]
   root 'gossips#index'
-  get 'welcome/:first_name', to: 'welcome#show'
-  get 'contact', to: 'contacts#show'
-  get 'team', to: 'team#show'
 end
 
